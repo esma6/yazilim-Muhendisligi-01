67f3586421bee6c7c1b136266cd1e46a02d12682
esma6 <77541320+esma6@users.noreply.github.com>
2022-05-05 17:37:54 +0300

    travis implemantation added.

---
+++ app/src/main/java/homework3/Car.java
@@ -0,0 +1,29 @@
+package homework3;
+public class Car {
+    private String name;
+    private int count;
+
+    public Car() {
+    }
+
+    public Car(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
